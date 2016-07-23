f8e31a1fce0aa9d4209ba469b661f5554d1c33d9
Fernando Pileggi <fpileggi@localhost.localdomain>
2016-07-23 14:28:15 -0300

    print token on screen

--- app/app.ts
+++ app/app.ts
@@ -4,7 +4,7 @@ import {StatusBar} from 'ionic-native';
 import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
 import {ListPage} from './pages/list/list';
 import {Push} from 'ionic-native';
-
+import {Storage, LocalStorage} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/app.html'
@@ -15,16 +15,6 @@ class MyApp {
   // make HelloIonicPage the root (or first) page
   rootPage: any = HelloIonicPage;
   pages: Array<{title: string, component: any}>;
-  push: any = Push.init({
-   android: {
-       senderID: "413993119939"
-   },
-   ios: {
-       alert: "true",
-       badge: true,
-       sound: 'false'
-   }
-  });
   constructor(
     private platform: Platform,
     private menu: MenuController
@@ -39,14 +29,28 @@ class MyApp {
   }
 
   initializeApp() {
-    this.push.on('registration', function(data) {
-    	console.log('Device Token:', data.registrationId);
-    });
-    this.push.on('error', function(e) {
-      console.log('Push Register Error', e.message);
-    });
-    
     this.platform.ready().then(() => {
+      let local = new Storage(LocalStorage);
+      let push = Push.init({
+       android: {
+           senderID: "651298752808"
+       },
+       ios: {
+           alert: "true",
+           badge: true,
+           sound: 'false'
+       }
+      });
+      push.on('registration', function(data) {
+        local.set('deviceToken', data.registrationId);
+      	console.log('Device Token:', data.registrationId);
+      });
+      push.on('error', function(e) {
+        console.log('Push Register Error', e.message);
+      });
+      push.on('notification', (data) => {
+          console.log('Message:', data);
+      });
       Push.hasPermission().then((data) => {
         console.log('isEnabled', data);
       });
--- app/pages/hello-ionic/hello-ionic.ts
+++ app/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,15 @@
 import {Component} from '@angular/core';
-
+import {Storage, LocalStorage} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
 })
 export class HelloIonicPage {
+  public deviceToken: any = '';
   constructor() {
-
+    let local = new Storage(LocalStorage);
+    local.get('deviceToken').then((value) => {
+      this.deviceToken = value;
+    });
   }
 }
